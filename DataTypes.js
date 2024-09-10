8fcd48d4af403491d92c071882cb8c86b5afca3c
HarshKorde5 <harshkorde05@gmail.com>
2024-09-10 22:53:20 +0530

    datatypes

---
+++ DataTypes.js
@@ -0,0 +1,27 @@
+
+"use strict"        //use modern js
+
+console.log(3
+    +4
+)       //avoid such use more readble normal code
+
+console.log("Harsh")
+
+let name = "Harsh"
+let age = 20
+let isLoggedIn = true
+
+//number => 2^53
+//bigInt
+//string = ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+
+log(typeof undefined)   //undefined
+log(typeof null)        //object
+
