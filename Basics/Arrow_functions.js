04c9ac85a6e613cce07a161d8669f866bf3a22d6
HarshKorde5 <harshkorde05@gmail.com>
2024-12-24 16:28:30 +0530

    arrow_functions

---
+++ Basics/Arrow_functions.js
@@ -0,0 +1,16 @@
+
+//explicit return is use of return keyword
+
+// const addTwo = (num1,num2) => {
+//     return num1 + num2;
+// }
+
+//implicit return is no use of return keyword
+// const addTwo = (num1,num2) => num1 + num2;
+
+// const addTwo = (num1,num2) => (num1 + num2);
+
+// const addTwo = (num1,num2) => {username : "harsh"};      //udefined due to absence of ()
+// const addTwo = (num1,num2) => ({username : "harsh"});
+
+console.log(addTwo(2,3));
